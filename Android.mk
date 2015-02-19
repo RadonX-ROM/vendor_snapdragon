8133b0f2bb151e3f7852dc76663837ae14137780
Your Name <you@example.com>
2015-02-19 17:13:00 +0100

    make installation easier

--- Android.mk
+++ Android.mk
@@ -1,11 +1,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
+clanglibs_path := $(LOCAL_PATH)/../../prebuilts/clang/linux-x86/host/llvm-Snapdragon_LLVM_for_Android_3.5/prebuilt/linux-x86_64/lib/clang/3.5.0/lib/
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.asan-arm-android
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux/libclang_rt.asan-arm-android.so
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.asan-arm-android.so
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -13,7 +15,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.builtins-aarch64
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux/libclang_rt.builtins-aarch64.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.builtins-aarch64.a
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -21,7 +23,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.builtins-arm_android
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux/libclang_rt.builtins-arm_android.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.builtins-arm_android.a
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -29,7 +31,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.profile-armv7
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux_armv7/libclang_rt.profile-armv7.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux_armv7/libclang_rt.profile-armv7.a
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -37,7 +39,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.optlibc-krait2
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux_krait2_optlibc/libclang_rt.optlibc-krait2.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux_krait2_optlibc/libclang_rt.optlibc-krait2.a
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -45,7 +47,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.translib
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux_translib/libclang_rt.translib.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux_translib/libclang_rt.translib.a
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -53,7 +55,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libclang_rt.translib32
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := /linux_translib/libclang_rt.translib32.a
+LOCAL_SRC_FILES := $(clanglibs_path)/linux_translib/libclang_rt.translib32.a
 
 include $(BUILD_STATIC_LIBRARY)
 
+clanglibs_path :=
