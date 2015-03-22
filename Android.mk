35173c5929a28fbad1807bd8cdff4c89dfd3852f
Your Name <you@example.com>
2015-03-22 16:51:52 +0100

    fixed adding prebuild libraries

--- Android.mk
+++ Android.mk
@@ -1,72 +1,117 @@
 ifeq ($(USE_CLANG_QCOM),true)
 LOCAL_PATH := $(call my-dir)
 
-clanglibs_path := $(LOCAL_PATH)/../../prebuilts/clang/linux-x86/host/llvm-Snapdragon_LLVM_for_Android_3.5/prebuilt/linux-x86_64/lib/clang/3.5.0/lib/
+clanglibs_path := /../../prebuilts/clang/linux-x86/host/llvm-Snapdragon_LLVM_for_Android_3.6/prebuilt/linux-x86_64/lib/clang/3.6.0/lib/
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libclang_rt.asan-arm-android
+#LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.asan-arm-android.so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+#include $(BUILD_PREBUILT)
+
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libclang_rt.san-arm-android
+##LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.san-arm-android.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libclang_rt.asan-arm-android
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.asan-arm-android.so
+##LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.asan-arm-android.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
-include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := libclang_rt.builtins-aarch64
-LOCAL_MODULE_TAGS := optional
+##LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.builtins-aarch64.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libclang_rt.builtins-arm_android
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.builtins-arm_android.a
-
-include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libclang_rt.builtins-arm-android
+##LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.builtins-arm-android.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libclang_rt.profile-arm-android
+#LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux/libclang_rt.profile-arm-android.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libclang_rt.profile-armv7
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux_armv7/libclang_rt.profile-armv7.a
+#LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux-propri_rt/libclang_rt.profile-armv7.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libclang_rt.optlibc-krait2
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux_krait2_optlibc/libclang_rt.optlibc-krait2.a
-
-include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libclang_rt.optlibc-krait
+#LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux-propri_rt/libclang_rt.optlibc-krait.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
-LOCAL_MODULE := libclang_rt.translib
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux_translib/libclang_rt.translib.a
-
-include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libclang_rt.translib64
+#LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux-propri_rt/libclang_rt.translib64.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libclang_rt.translib32
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(clanglibs_path)/linux_translib/libclang_rt.translib32.a
-
-include $(BUILD_STATIC_LIBRARY)
+#LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(clanglibs_path)/linux-propri_rt/libclang_rt.translib32.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(BUILD_PREBUILT)
 
 ifeq ($(USE_CLANG_QCOM_LTO),true)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := libLTO
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(clanglibs_path)/../../../libLTO.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_EXPORT_C_INCLUDES := $(clanglibs_path)/../include
+include $(PREBUILT_SHARED_LIBRARY)
 
-include $(BUILD_SHARED_LIBRARY)
 endif
 
 clanglibs_path := 
